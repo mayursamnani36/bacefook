a5277aff85fec4c8948a92b104cb6c1e82650df2
Mayur Samnani <mayursamnani36@gmail.com>
2020-03-06 22:25:31 +0530

    Added Login and Logout functionality with restrictions

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +10,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account Created for {username}! You can now login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
