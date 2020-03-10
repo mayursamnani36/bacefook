9ab165f61b2a82034445dc19cfee6a4f021f5875
Mayur Samnani <mayursamnani36@gmail.com>
2020-03-10 19:41:20 +0530

    added .gitignore file and requirements file

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.png', upload_to='profile_pics')
@@ -9,10 +10,10 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 and img.width > 300:
-            output_size = (300,300)
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
