800052037d0f775cbc81da84831dedd4f6f08b4e
mayursamnani36 <54547503+mayursamnani36@users.noreply.github.com>
2020-07-15 22:16:09 +0530

    Deleted virtualenv folder and updated gitigonre

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mayur/Projects and stuff/Django Projects/bacefook/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
