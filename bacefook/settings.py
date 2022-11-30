cbadd7af0756fd556d4144eccb62e1770b568835
mayursamnani36 <mayursamnani36@gmail.com>
2022-11-30 10:02:55 +0530

    Update settings.py

--- bacefook/settings.py
+++ bacefook/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'samnanimayur784@gmail.com'
+EMAIL_HOST_USER = 'ENTER YOUR EMAIL HERE AND DO NOT USE MINE IF YOU HAVE CLONED THIS REPO'
 EMAIL_HOST_PASSWORD = 'apsnpbwqszgrujke'
